e043bcb68d6adfabc3e9cf4da8e279eb3920d422
samuel21695 <samuel21695@gmail.com>
2023-11-03 15:50:54 +0900

    ✨ const path & body parsing

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000; // port number
 
 // set middleware for using static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'static')));
+
+// set middleware for body parsing from POST request 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/index.html');
+  res.sendFile(path.join(__dirname,'static', 'index.html'));
 });
 
 app.listen(port, () => {
