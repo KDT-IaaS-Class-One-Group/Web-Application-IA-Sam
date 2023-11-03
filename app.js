bfd2f5163fae9fdb0b2095254dd648370d864402
samuel21695 <samuel21695@gmail.com>
2023-11-03 16:04:06 +0900

    ✅ test loading css file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const port = 3000; // port number
 
 // set middleware for using static files
-app.use(express.static(path.join(__dirname,'static')));
+app.use('/static', express.static(path.join(__dirname,'static')));
 
 // set middleware for body parsing from POST request 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'static', 'index.html'));
