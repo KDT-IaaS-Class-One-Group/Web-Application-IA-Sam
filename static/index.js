ff1942af91375f169d231919ab9362e15114a6e6
samuel21695 <samuel21695@gmail.com>
2023-11-04 18:40:34 +0900

    ✅ test messageText succesfully loaded

--- static/index.js
+++ static/index.js
@@ -3,5 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const messageForm = document.getElementById('messageForm');
   const text = document.getElementById('text');
   const submit = document.getElementById('submit');
-  
+
+  // create li tags with textcontents
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent the form from restarting and actually submitting webpage 
+
+    const messageText = text.value;
+    console.log(messageText)
+  })
 })
