342263f58182e24d07c89d3edf1532b69b12f98a
samuel21695 <samuel21695@gmail.com>
2023-11-06 12:41:24 +0900

    ✨ feat: post method check
    also delete event.preventDefault to use POST method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const fs = require('fs') // file system module
 const app = express();
 const port = 3000; // port number
 
@@ -14,6 +15,13 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'static', 'index.html'));
 });
+
+//
+app.post('/submit', (req, res) => {
+  const userMessage = req.body.message;
+
+  res.json({ message: 'message has successfully delivered'});
+})
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
