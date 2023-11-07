df6edcd9acaf4fb1d46aba52c11b85b09e132a9d
samuel21695 <samuel21695@gmail.com>
2023-11-07 12:09:20 +0900

    ✨ feat: fetch to send its data with POST req

--- static/index.js
+++ static/index.js
@@ -4,9 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const message = document.getElementById('message');
   const mainDiv = document.getElementById('main');
 
+  // function dealing with POST request
+  async function postData() {
+    try {
+      const response = await fetch('/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: messageText}), //send message via JSON
+      });
+
+      if (response.ok) {
+        const data = await response.json(); // parse res data from the server
+        console.log(data.message);
+      } else {
+        console.error('POST request failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
   // create li tags with textcontents
   messageForm.addEventListener("submit", () => {
-
 
     const messageText = message.value;
     if (messageText) {
@@ -20,5 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // Clear the input field after the message is sent
       message.value = "";
     }
+    
+    postData(); // send POST request
   })
 })
